ad21412de9a3a9cdf99ce68fc6a8a23c73a4901c
p i e r o k <82934655+pierokchad@users.noreply.github.com>
2024-03-31 22:00:02 +0200

    .
    .

--- src/dist/index.js
+++ src/dist/index.js
@@ -3,4 +3,3 @@ Object.defineProperty(exports, "__esModule", { value: true });
 
 var command_1 = require("./structures/command");
 Object.defineProperty(exports, "Command", { enumerable: true, get: function () { return command_1.Command; } });
-sourceMappingURL=index.js.map
